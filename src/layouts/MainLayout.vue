ece7d282de0827a27dea4f7ee24cde65c1bf2260
ivancr2 <ivancalixto1@gmail.com>
2020-12-01 14:48:10 -0300

    primeiro esboço layout tela principal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,39 +1,45 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header elevated>
+  <q-layout view="lHh Lpr lFf">
+    <q-header reveal bordered class="bg-primary text-white">
       <q-toolbar>
-        <img
-          src="logo_dislexia.jfif"
-          alt="Logo Dislexia"
-          id="logoDislexia"
-          clickable
-          v-ripple
-          @click="$router.replace('/')"
-        />
         <q-toolbar-title
-          id="tituloGeral"
           clickable
           v-ripple
           @click="$router.replace('/')"
+          active-class="my-menu-link"
         >
-          App Dislexia
+          <div style="width:110px; float:left; ">
+            <img
+              style="display:block; margin-right:auto;  "
+              width="100px"
+              src="logo_dislexia.jfif"
+            />
+          </div>
+          <div
+            style="width:50%; margin-left:50%; margin-right:50%; word-break: break-all; padding-top: 12px"
+          >
+            <!--<span style="font-size:15px; "
+              >App Balnealidade das Praias do Estado de São Paulo</span
+            >-->
+            <span style="font-size:15px; ">App Dislexia</span>
+            <br />
+          </div>
         </q-toolbar-title>
-        <q-btn id="botaoSobre" icon="info" to="/sobre" flat />
+
+        <q-btn icon="info" to="/sobre" flat />
       </q-toolbar>
     </q-header>
 
-    <q-footer>
+    <q-footer elevated>
       <q-toolbar>
         <q-toolbar-title>
-          <div>
+          <div style="min-width:40px;width:10%; float:left; ">
             <img
-              id="logoEpm"
-              src="epm.png"
               style="margin-top: 5px; "
               width="40px"
+              src="~assets/logo_epm.png"
             />
           </div>
-
           <div
             class="desktop-only text-caption text-center float-left"
             style="width:80%"
@@ -41,13 +47,16 @@
             Rua Botucatu, 862-Térreo, Vila Clementino, São Paulo (SP). Cep:
             04023-062 - Email: contato@dis.epm.br - CNPJ:60.453.032/0001-74
           </div>
-          <img
-            id="logoUnifesp"
+          <div
             class="text-right"
-            src="logo_unifesp.png"
-            style="margin-top: 5px; "
-            width="75px"
-          />
+            style="min-width:75px; width:10%;float:right;"
+          >
+            <img
+              style="margin-top: 5px; "
+              width="75px"
+              src="~assets/logo_unifesp.png"
+            />
+          </div>
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
